ca1940e5b1cc99756eee4e3a6b9aaba5384b04b2
Bill <bill@thejavashop.net>
2014-02-22 20:48:17 -0600

    Issue #20 - Remove unused fields

--- src/main/java/org/bff/javampd/processor/CommentProcessor.java
+++ src/main/java/org/bff/javampd/processor/CommentProcessor.java
@@ -1,11 +1,8 @@
 package org.bff.javampd.processor;
 
 import org.bff.javampd.objects.MPDSong;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class CommentProcessor extends SongResponseProcessor {
-    private static Logger logger = LoggerFactory.getLogger(CommentProcessor.class);
 
     public CommentProcessor() {
         super("Comment:");
--- src/main/java/org/bff/javampd/processor/PositionProcessor.java
+++ src/main/java/org/bff/javampd/processor/PositionProcessor.java
@@ -1,11 +1,8 @@
 package org.bff.javampd.processor;
 
 import org.bff.javampd.objects.MPDSong;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class PositionProcessor extends SongResponseProcessor {
-    private static Logger logger = LoggerFactory.getLogger(PositionProcessor.class);
 
     public PositionProcessor() {
         super("Pos:");
