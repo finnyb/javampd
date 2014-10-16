761d428b6174538644cb20187c311fe3f73c8b75
Bill <bill@thejavashop.net>
2014-10-16 15:39:36 -0500

    Issue #39 - properly name integration test

--- src/it/java/org/bff/javampd/genre/MPDGenreDatabaseIT.java
+++ src/it/java/org/bff/javampd/genre/MPDGenreDatabaseIT.java
@@ -7,7 +7,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class MPDGenreDatabaseTest extends BaseTest {
+public class MPDGenreDatabaseIT extends BaseTest {
     private GenreDatabase genreDatabase;
 
     @Before
