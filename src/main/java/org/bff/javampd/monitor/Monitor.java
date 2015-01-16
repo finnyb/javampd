6bb949ca7a2dcc254710b63033cda53d87cf8e21
Bill <bill@thejavashop.net>
2015-01-15 21:20:14 -0600

    remove unused import

--- src/main/java/org/bff/javampd/monitor/Monitor.java
+++ src/main/java/org/bff/javampd/monitor/Monitor.java
@@ -1,12 +1,8 @@
 package org.bff.javampd.monitor;
 
-import org.bff.javampd.MPDException;
-
 public interface Monitor {
     /**
      * Check the status
-     *
-     * @throws MPDException if there are issues that occur during the check
      */
     void checkStatus();
 }
